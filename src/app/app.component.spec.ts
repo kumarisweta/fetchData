f8645dd9c686b7c4bfa960b4e0b334cb102d70ea
Sweta Kumari <ksweta102@gmail.com>
2023-10-18 19:42:40 +0300

    FetchData Assignment

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,52 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
-  }));
+  let fixture: ComponentFixture<AppComponent>;
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [AppComponent],
+    });
+
+    fixture = TestBed.createComponent(AppComponent);
   });
 
-  it(`should have as title 'fetchData'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('fetchData');
+  it('should create the component', () => {
+    expect(fixture.componentInstance).toBeTruthy();
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('fetchData app is running!');
-  });
+  // it('should have a title', () => {
+  //   const compiled = fixture.debugElement.nativeElement;
+  //   expect(compiled.querySelector('h1').textContent).toContain('Welcome');
+  // });
 });
+
+
+
+
+
+
+// import { TestBed } from '@angular/core/testing';
+// import { RouterTestingModule } from '@angular/router/testing';
+// import { AppComponent } from './app.component';
+
+// describe('AppComponent', () => {
+//   beforeEach(() => TestBed.configureTestingModule({
+//     imports: [RouterTestingModule],
+//     declarations: [AppComponent]
+//   }));
+
+//   it('should create the app', () => {
+//     const fixture = TestBed.createComponent(AppComponent);
+//     const app = fixture.componentInstance;
+//     expect(app).toBeTruthy();
+//   });
+
+//   // it('should render title', () => {
+//   //   const fixture = TestBed.createComponent(AppComponent);
+//   //   fixture.detectChanges();
+//   //   const compiled = fixture.nativeElement as HTMLElement;
+//   //   expect(compiled.querySelector('.content span')?.textContent).toContain('fetchData app is running!');
+//   // });
+// });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,72 @@
-import { Component } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
-  title = 'fetchData';
+export class AppComponent implements OnInit {
+  public tableList: any;
+  public filteredList: any;
+  public isLoading: boolean;
+  public showError: boolean;
+
+  constructor(private httpClient: HttpClient) {
+    this.tableList = [];
+    this.filteredList = [];
+    this.isLoading = true;
+    this.showError = false;
+  }
+  ngOnInit(): void {
+    this.gettableList();
+  }
+
+  gettableList() {
+    this.httpClient
+      .get(
+        "https://stage-live.testenvs.dev/api/v1/live/partner/lobby?currency=mBTC&partnerId=60fe77298861f37264ac3b72"
+      )
+      .subscribe(
+        (result: any) => {
+          this.tableList = Object.values(result.tables);
+          this.filteredList = this.tableList;
+          this.isLoading = false;
+        },
+        (error) => {
+          this.showError = true;
+          console.log(
+            "Something went wrong, Please try again... ",
+            error.message
+          );
+        }
+      );
+  }
+
+  searchTerm = "";
+  filterTables() {
+    this.filteredList = this.tableList.filter(
+      (t: {
+        tableId: string;
+        gameProvider: string;
+        gameType: string;
+        name: string;
+        currency: string;
+      }) =>
+        t.tableId.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
+        t.gameProvider.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
+        t.gameType.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
+        t.name.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
+        t.currency.toLowerCase().includes(this.searchTerm.toLowerCase())
+    );
+  }
+
+  shouldShowEllipsis(name: string): boolean {
+    return name.split(" ").length > 4;
+  }
+
+  shortenName(name: string): string {
+    const words = name.split(" ");
+    return words.slice(0, 4).join(" ") + "...";
+  }
 }
